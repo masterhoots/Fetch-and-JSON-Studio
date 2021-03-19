393e8b1dd3597a57b52f709929a3d172e1405229
Brian Hoots <thehoots26@gmail.com>
2021-03-18 20:38:17 -0500

    Somebody Fetch Me

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 // TODO: add code here
+
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( response => {
+
+  response.json().then( json => {
+
+    let div = document.getElementById("container");
+
+    let astronaut = '';
+
+    json.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+
+    // console.log(json[1]);
+    for (let i = 0; i < json.length; i++) {
+        
+        let activeColor;
+
+        if (json[i].active === true) {
+
+            activeColor = "color: green";
+            console.log("whatever")
+            
+        } else {
+
+            activeColor = "color: black";
+            
+        }
+
+        astronaut += `<div class="astronaut">
+        <div class="bio">
+           <h3>${json[i].firstName} ${json[i].lastName}</h3>
+           <ul>
+              <li>Hours in space: ${json[i].hoursInSpace}</li>
+              <li style="${activeColor}">Active: ${json[i].active}</li>
+              <li>Skills: ${json[i].skills}</li>
+           </ul>
+        </div>
+        <img class="avatar" src="${json[i].picture}">
+     </div>`;
+        console.log(div)
+
+    }
+    astronaut += `<p>Astronaut Count: ${json.length}</p>`
+    div.innerHTML = astronaut;
+  } )
+    
+}) 
